725ebc9713c2b63d97a7ea5fc3ae34e6a83f8368
Jonathan Kaneshiro <jkaneshiro97@gmail.com>
2019-12-15 21:12:59 -0800

    finished

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -2,12 +2,73 @@ package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class  MainActivity extends AppCompatActivity {
+
+    private TextView questionText;
+    private Button trueButton, falseButton, nextButton;
+
+    private int currIndex = 0;
+
+    private TrueFalse[] questions = new TrueFalse[] {
+
+            new TrueFalse(R.string.question01, true),
+            new TrueFalse(R.string.question02, false),
+            new TrueFalse(R.string.question03, true),
+            new TrueFalse(R.string.question04, true),
+            new TrueFalse(R.string.question05, false),
+            new TrueFalse(R.string.question06, true),
+            new TrueFalse(R.string.question07, true),
+            new TrueFalse(R.string.question08, false)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        questionText = (TextView)findViewById(R.id.question_text_view);
+        final int Question = questions[currIndex].getQuestion();
+        questionText.setText(Question);
+
+        trueButton = (Button)findViewById(R.id.true_button);
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(true);
+            }
+        });
+
+        falseButton = (Button)findViewById(R.id.false_button);
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(false);
+            }
+        });
+
+        nextButton = (Button)findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currIndex = (currIndex + 1) % questions.length;
+                int question = questions[currIndex].getQuestion();
+                questionText.setText(question);
+            }
+        });
+    }
+
+    private void checkAnswer(boolean userPressed) {
+        boolean answer = questions[currIndex].isTrueQuestion();
+
+        if(userPressed == answer) {
+            Toast.makeText(MainActivity.this, R.string.true_message, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(MainActivity.this, R.string.false_message, Toast.LENGTH_SHORT).show();
+        }
     }
 }
---
+++ app/src/main/java/com/example/quizapp/TrueFalse.java
@@ -0,0 +1,28 @@
+package com.example.quizapp;
+
+public class TrueFalse {
+
+    private int question;
+    private boolean trueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        this.question = question;
+        this.trueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(int question) {
+        this.question = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return trueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        this.trueQuestion = trueQuestion;
+    }
+}
